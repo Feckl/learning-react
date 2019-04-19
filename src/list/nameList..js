0fe306aa6421141d31f003dbbecf2292502c5c3f
Feckl <shengyongfei@hotmail.com>
2019-04-20 00:58:39 +0800

    create react

---
+++ src/list/nameList..js
@@ -0,0 +1,11 @@
+import React, { Compunent, Fragment } from 'react'
+
+export default class NameList extends Compunent {
+  reader() {
+    return (
+      <Fragment>
+        
+      </Fragment>
+    )
+  }
+}
