01d4f57cf678907eb408566ccfe55601e8cb7234
Feckl <shengyongfei@hotmail.com>
2019-04-21 01:47:23 +0800

    ''

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import NameList from './list/nameList.';
+import NameList from './list/nameList';
 
 ReactDOM.render(<NameList />, document.getElementById('root'));
 
