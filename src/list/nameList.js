bfdd2fe569e458e008deb59f212872c8f9a85595
Feckl <shengyongfei@hotmail.com>
2019-04-22 18:32:37 +0800

    ''

---
+++ src/list/nameList.js
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from 'react'
+
+class NameList extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div>123</div>
+      </Fragment>
+    )
+  }
+}
+export default NameList
